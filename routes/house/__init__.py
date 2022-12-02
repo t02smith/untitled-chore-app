0a58e7f02ef76cd6a799a95709718ea2441af05f
Tom Smith <thomas-smith@sky.com>
2022-12-02 19:58:32 +0000

    fixed import issue for lib.db.user.User

--- routes/house/__init__.py
+++ routes/house/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends
 from lib.db import home
-from lib.auth.user import get_current_active_user, User
+from lib.auth.user import get_current_active_user
+from lib.db.user import User
 
 router = APIRouter(prefix="/house")
 
+
 @router.post("/")
 async def create_home(
-  newhome: home.HomeIn, user: User = Depends(get_current_active_user)
+    newhome: home.HomeIn, user: User = Depends(get_current_active_user)
 ):
-  await home.register_home(newhome, user.username)
+    await home.register_home(newhome, user.username)
