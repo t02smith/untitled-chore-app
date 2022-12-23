60dd5ebe2bbf40d76b1cee85910f7673a858055c
Tom Smith <thomas-smith@sky.com>
2022-12-23 15:01:00 +0000

    added skeleton create home page

---
+++ src/components/ChoreCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card text-white bg-dark shadow" style="width: 20rem">
+    <div class="card-body">
+      <div class="d-flex gap-3 align-items-center">
+        <img :src="props.icon" alt="" class="rounded-full" />
+        <div class="flex-grow-1">
+          <h5 class="card-title">{{ props.name }}</h5>
+          <h6
+            class="card-subtitle"
+            style="margin-top: -0.5rem"
+            :style="`color: ${props.color};`"
+          >
+            {{ props.room }}
+          </h6>
+        </div>
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          class="form-check-input ms-2 bg-secondary justify-self-end"
+          style="width: 2rem; height: 2rem"
+          @click="onToggle"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  room: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: "#ccc",
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+  onToggle: {
+    type: Function,
+  },
+});
+</script>
--- src/views/home/CreateHome.vue
+++ src/views/home/CreateHome.vue
@@ -1,3 +1,161 @@
 <template>
-  <div></div>
+  <div class="container p-4">
+    <div class="jumbotron bg-dark p-4 my-4 shadow rounded">
+      <h1 class="display-2 text-primary font-weight-bold">
+        Create a new home!
+      </h1>
+      <p class="lead">
+        Fill in the fields below to create a new home, add some chores, invite
+        your housemates and jumpstart your productivity!
+      </p>
+    </div>
+
+    <form
+      @submit.prevent=""
+      class="d-flex flex-column gap-5"
+      style="max-width: 992px"
+    >
+      <div class="form-group">
+        <label for="" class="h5 text-primary">Your house name:</label>
+        <p id="helpId" class="text-muted" style="margin-top: -0.5rem">
+          This has to be different from all your other houses
+        </p>
+        <input
+          type="text"
+          name=""
+          id=""
+          class="form-control"
+          placeholder="e.g. myhome"
+          aria-describedby="helpId"
+          v-model="houseName"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="" class="h5 text-primary">Pick some basic chores:</label>
+        <p class="text-muted" style="margin-top: -0.5rem">
+          Or design your own once you've created your house
+        </p>
+        <div class="chore-options">
+          <ChoreCard
+            v-for="c in chores"
+            :name="c.name"
+            :room="c.room"
+            :color="c.color"
+            :icon="c.icon"
+            :onToggle="() => toggleChore(c.id)"
+          />
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="" class="h5 text-primary">Invite some friends:</label>
+        <p class="text-muted" style="margin-top: -0.5rem">
+          Sent your housemates an invite to join once your house is created
+        </p>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">@</span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            v-model="userToInvite"
+            @submit="console.log('hello')"
+          />
+        </div>
+
+        <div class="d-flex gap-2">
+          <button
+            class="btn btn-primary font-weight-bold px-2 py-1"
+            v-for="user in invitedUsers"
+          >
+            <small>{{ user }}</small>
+          </button>
+        </div>
+      </div>
+
+      <button
+        type="submit"
+        class="btn btn-success btn-lg"
+        style="width: fit-content"
+      >
+        Create
+      </button>
+    </form>
+  </div>
 </template>
+<script setup>
+import { ref } from "vue";
+import ChoreCard from "../../components/ChoreCard.vue";
+
+const houseName = ref("");
+const chosenChores = ref([]);
+
+const userToInvite = ref("");
+const invitedUsers = ref(["t02smith", "tcs1g20"]);
+
+const toggleChore = (id) =>
+  chosenChores.value.includes(id)
+    ? (chosenChores.value = chosenChores.value.filter((c) => c !== id))
+    : chosenChores.value.push(id);
+
+const chores = [
+  {
+    id: 0,
+    name: "Clean the Shower",
+    room: "Bathroom",
+    color: "#0ac",
+    icon: "/src/assets/svg/bath.svg",
+  },
+  {
+    id: 1,
+    name: "Do the Dishes",
+    room: "Kitchen",
+    color: "#F24E1E",
+    icon: "/src/assets/svg/kitchen.svg",
+  },
+  {
+    id: 2,
+    name: "Hoover",
+    room: "Living Room",
+    color: "#0FA958",
+    icon: "/src/assets/svg/lounge.svg",
+  },
+  {
+    id: 3,
+    name: "Take the Bins Out",
+    room: "Kitchen",
+    color: "#F24E1E",
+    icon: "/src/assets/svg/kitchen.svg",
+  },
+  {
+    id: 4,
+    name: "Tidy Up",
+    room: "Living Room",
+    color: "#0FA958",
+    icon: "/src/assets/svg/lounge.svg",
+  },
+  {
+    id: 5,
+    name: "Clean the Toilet",
+    room: "Bathroom",
+    color: "#0ac",
+    icon: "/src/assets/svg/bath.svg",
+  },
+];
+
+// TODO fetch users existing house names to validate new house name
+// TODO fetch default chores from api
+</script>
+<style scoped>
+input[type="text"] {
+  max-width: 600px;
+}
+
+.chore-options {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1rem;
+  place-items: center;
+}
+</style>
