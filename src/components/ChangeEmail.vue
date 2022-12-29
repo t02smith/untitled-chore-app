9f18b9d57ceb98c849d946d42bb9a204852b000f
Julia Lindgren <julia.r.lindgren@gmail.com>
2022-12-29 12:51:53 +0000

    Add change email and navigation to user page

---
+++ src/components/ChangeEmail.vue
@@ -0,0 +1,31 @@
+<script setup>
+// TODO state logic
+</script>
+
+<template>
+    <div class="changeEmail">
+        
+        <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">New Email</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="New Email">
+        </div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Current Password</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Current Password">
+        </div>
+        <button type="button" class="btn btn-primary"
+        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .99rem;">
+        Save
+        </button>
+    </div>
+    
+</template>
+
+<style scoped>
+
+    div.changeEmail {
+        left: 100%;
+        top: 50%;
+    }
+
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
+import UserNavbar from "../components/UserNavbar.vue";
 </script>
 
 <template>
     <main>
         <Navbar />
+        <UserNavbar />
     </main>
 </template>
 
