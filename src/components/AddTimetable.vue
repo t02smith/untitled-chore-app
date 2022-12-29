072bc48bf41d86e22cc96ea7543ee0c23fc19922
Julia Lindgren <julia.r.lindgren@gmail.com>
2022-12-29 14:04:42 +0000

    Add add timetable to user page

---
+++ src/components/AddTimetable.vue
@@ -0,0 +1,26 @@
+<script setup>
+// TODO state logic
+</script>
+
+<template>
+    <div class="addTimetable">
+        
+        <h1 class="display-6">Add Timetable</h1>
+        <!-- Template for now, change when decide how handle timetables -->
+        <div class="d-grid gap-2 col-7 mx-auto">
+            <button class="btn btn-primary" type="button">Import</button>
+            <button class="btn btn-primary" type="button">Delete</button>
+        </div>  
+        
+    </div>
+    
+</template>
+
+<style scoped>
+
+    div.addTimetable {
+        left: 100%;
+        top: 50%;
+    }
+
+</style>
--- src/components/UserNavbar.vue
+++ src/components/UserNavbar.vue
@@ -2,6 +2,7 @@
     import ChangeEmail from "../components/ChangeEmail.vue";
     import ChangePassword from "../components/ChangePassword.vue";
     import DeleteAccount from "../components/DeleteAccount.vue";
+    import AddTimetable from "../components/AddTimetable.vue";
 </script>
 
 <template>
@@ -10,13 +11,13 @@
             <button class="nav-link active" id="v-pills-changeEmail-tab" data-bs-toggle="pill" data-bs-target="#v-pills-changeEmail" type="button" role="tab" aria-controls="v-pills-changeEmail" aria-selected="true">Change Email</button>
             <button class="nav-link" id="v-pills-changePassword-tab" data-bs-toggle="pill" data-bs-target="#v-pills-changePassword" type="button" role="tab" aria-controls="v-pills-changePassword" aria-selected="false">Change Password</button>
             <button class="nav-link" id="v-pills-deleteAccount-tab" data-bs-toggle="pill" data-bs-target="#v-pills-deleteAccount" type="button" role="tab" aria-controls="v-pills-deleteAccount" aria-selected="false">Delete Account</button>
-            <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+            <button class="nav-link" id="v-pills-addTimetable-tab" data-bs-toggle="pill" data-bs-target="#v-pills-addTimetable" type="button" role="tab" aria-controls="v-pills-addTimetable" aria-selected="false">Add Timetable</button>
         </div>
         <div class="tab-content" id="v-pills-tabContent">
             <div class="tab-pane fade show active" id="v-pills-changeEmail" role="tabpanel" aria-labelledby="v-pills-changeEmail-tab" tabindex="0"><ChangeEmail /></div>
             <div class="tab-pane fade" id="v-pills-changePassword" role="tabpanel" aria-labelledby="v-pills-changePassword-tab" tabindex="0"><ChangePassword /></div>
             <div class="tab-pane fade" id="v-pills-deleteAccount" role="tabpanel" aria-labelledby="v-pills-deleteAccount-tab" tabindex="0"><DeleteAccount /></div>
-            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">...</div>
+            <div class="tab-pane fade" id="v-pills-addTimetable" role="tabpanel" aria-labelledby="v-pills-addTimetable-tab" tabindex="0"><AddTimetable /></div>
         </div>
     </div>
 </template>
