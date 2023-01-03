1f4c5e4daf42dc6358ffa46abbfc4ae96ed345e0
vzz1g20 <vzz1g20@soton.ac.uk>
2023-01-03 17:59:42 +0200

    Added some design for chat box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
+import ChatBar from "../components/ChatBar.vue";
 </script>
 
 <template>
     <main>
         <Navbar />
+        <ChatBar />
     </main>
 </template>
 
